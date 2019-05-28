f31f9bd55fa0c12ee8614cc353e9754c76356057
tani <tani@1-10.com>
2019-05-28 13:27:15 +0900

    [update]

---
+++ src/DataGrid.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="grid">
+    <div v-for="(item, index) in list" :key="index" class="item">
+      <img :src="item.img">
+      <p>{{ item.text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    list: Array
+  },
+  computed: {
+    gridType(type) {
+      switch(type) {
+        case 'normal':
+          return ''
+        case 'random':
+          return ''
+
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 30px;
+
+  img {
+    width: 100%;
+  }
+}
+</style>
