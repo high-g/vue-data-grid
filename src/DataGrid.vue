baa2a645f9586b1d603acbae063f59a199176ba6
tani <tani@1-10.com>
2019-05-29 20:14:38 +0900

    [update]random実装

--- src/DataGrid.vue
+++ src/DataGrid.vue
@@ -1,6 +1,11 @@
 <template>
-  <div :class="gridType">
-    <div v-for="(item, index) in list" :key="index" class="item">
+  <div :class="gridType" :style="randomGridTemplates">
+    <div
+      v-for="(item, index) in list"
+      :key="index"
+      class="item"
+      :style="randomGridArea(index + 1)"
+    >
       <img :src="item.img">
       <p>{{ item.text }}</p>
     </div>
@@ -13,7 +18,16 @@ export default {
     list: Array,
     grid: String
   },
+  data() {
+    return {
+      randomArray: [],
+      randomColumnsMaxCount: 4
+    }
+  },
   computed: {
+    /**
+     * grid切り分け
+     */
     gridType() {
       const grid = this.grid
       if(
@@ -25,12 +39,161 @@ export default {
       } else {
         return 'normal'
       }
+    },
+
+    /**
+     * ランダムgrid
+     */
+    randomGridTemplates() {
+      let strTemplates = ''
+
+      if(this.grid === 'random') {
+        strTemplates = 'grid-template-areas:'
+        const arr = this.randomArray
+
+        for(let y=0; y<arr.length; y++) {
+          if(arr[y].length > 0) {
+            strTemplates += '"'
+          }
+
+          for(let x=0; x<this.randomColumnsMaxCount; x++) {
+            if(arr[y][x]) {
+              strTemplates += ' area' + arr[y][x]
+            } else if(arr[y].length > 0) {
+              strTemplates += ' .'
+            }
+          }
+
+          if(arr[y].length > 0) {
+            strTemplates += '" '
+          }
+        }
+        strTemplates += ';'
+
+        console.log(strTemplates);
+      }
+
+      return strTemplates
+    },
+
+    /**
+     * ランダムgridArea
+     */
+    randomGridArea() {
+      return (num) => {
+        let strArea = ''
+
+        if(this.grid === 'random') {
+          strArea = 'grid-area: area' + num + ';';
+          console.log('strArea',strArea)
+        }
+        return strArea
+      }
     }
   },
-  mounted() {
-    console.log('this.list',this.list)
+  created() {
     if(this.grid === 'random') {
-      console.log(1)
+      this.createRandomArray(this.list.length)
+    }
+  },
+  methods: {
+    /**
+     * 描画用二次元配列作成
+     * @param len {Number}
+     */
+    createRandomArray(len) {
+      let pos = {}
+      let rect = {}
+
+      for(let i = 1; i <= len; i++) {
+        this.adjustRandomArray()
+        pos = this.getRandomPosition()
+        rect = this.getDrawRect(pos)
+        this.setRandomArray(pos, rect, i)
+      }
+      console.log('randomArray', this.randomArray);
+    },
+
+    /**
+     * 二次元配列格納領域調整
+     */
+    adjustRandomArray() {
+      const arr = this.randomArray
+      const len = arr.length
+      let lastRow = 0
+
+      // 値が入っている最終行を導く
+      if(len === 0) {
+        this.randomArray[0] = []
+        this.randomArray[1] = []
+      } else {
+        for(let i=0; i<len; i++) {
+          if(arr[i].length > 0) {
+            lastRow = i;
+          }
+        }
+        this.randomArray[lastRow + 1] = []
+        this.randomArray[lastRow + 2] = []
+      }
+    },
+
+    /**
+     * 二次元配列格納領域調整
+     */
+    getRandomPosition() {
+      const arr = this.randomArray
+      const len = arr.length
+      const max = this.randomColumnsMaxCount
+
+      for(let y=0; y<len; y++) {
+        for(let x=0; x<max; x++) {
+          if(!arr[y][x]) {
+            return {x, y}
+          }
+        }
+      }
+    },
+
+    /**
+     * 格納対象の縦横決定
+     * @param pos {Object}
+     */
+    getDrawRect(pos) {
+      return {
+        w: (pos.x === this.randomColumnsMaxCount - 1) ? 1 : Math.floor((Math.random() * 2) + 1),
+        h: Math.floor((Math.random() * 2) + 1)
+      }
+    },
+
+    /**
+     * 格納対象の縦横決定
+     * @param pos {Object}
+     * @param rect {Object}
+     * @param num {Number}
+     */
+    setRandomArray(pos, rect, num) {
+      this.randomArray[pos.y][pos.x] = num
+
+      if(
+        rect.w > 1 &&
+        !this.randomArray[pos.y][pos.x + 1]
+      ) {
+        this.randomArray[pos.y][pos.x + 1] = num
+      }
+      if(
+        rect.h > 1 &&
+        !this.randomArray[pos.y + 1][pos.x]
+      ) {
+        this.randomArray[pos.y + 1][pos.x] = num
+      }
+      if(
+        rect.w > 1 && rect.h > 1 &&
+        !this.randomArray[pos.y + 1][pos.x + 1] &&
+        this.randomArray[pos.y][pos.x + 1] === num &&
+        this.randomArray[pos.y + 1][pos.x] === num
+      ) {
+        this.randomArray[pos.y + 1][pos.x + 1] = num
+      }
     }
   }
 }
